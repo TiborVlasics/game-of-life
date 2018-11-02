4304c3db0850c45c7832c49fafcaaa7b79510a9b
TiborVlasics <tibor.vlasics@gmail.com>
2018-11-02 14:18:26 +0100

    Add function that maps grid dom element to matrix
    The matrix will be needed to make cells evolve and later I will map them
    back as dom elements

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ function createGrid(height, width) {
   return grid;
 }
 
+function mapGridToMatrix(grid) {
+  let matrix = [];
+  let rows = grid.children;
+
+  for (let i = 0; i < rows.length; i++) {
+    matrix.push([]);
+    let cells = rows[i].children;
+
+    for (let j = 0; j < cells.length; j++) {
+      let cell = cells[j];
+      if (cell.className === "cell") {
+        matrix[i].push(0);
+      } else if (cell.className === "cell living") {
+        matrix[i].push(1);
+      }
+    }
+  }
+
+  return matrix;
+}
+
 function main() {
   const root = document.querySelector("#root");
   const grid = createGrid(20, 15);
